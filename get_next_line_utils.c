a6ca39d569800c069fbb6b0a57c9402cbd9d0d64
MehdiByteByByte <holoyoda@gmail.com>
2024-02-10 16:49:17 +0100

    utils

---
+++ get_next_line_utils.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mboughra <mboughra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 15:57:04 by mboughra          #+#    #+#             */
+/*   Updated: 2024/01/17 21:37:14 by mboughra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char	*dst, char	*src, size_t	dstsize)
+{
+	size_t	i;
+	char	*s;
+
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	s = (char *)src;
+	i = 0;
+	while (src[i] && i < (dstsize -1))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	while (src[i])
+		i++;
+	return (i);
+}
+
+int	newcheck(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strdup(char *s1)
+{
+	size_t	i;
+	size_t	lenofs;
+	char	*s2;
+
+	i = 0;
+	lenofs = ft_strlen(s1)+1;
+	s2 = malloc(lenofs * sizeof(char));
+	if (!s2)
+		return (NULL);
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
